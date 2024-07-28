00bdaac3fab704fb6619960ad34e906af8467762
JP Whitaker <jpw@hawaii.edu>
2024-07-28 00:14:22 -1000

    got scaled unit circle points rendered in quiz view

--- Maru/Views/QuizView.swift
+++ Maru/Views/QuizView.swift
@@ -28,7 +28,7 @@ struct QuizView: View {
         .padding(10)
       Spacer()
       AnswerSelection()
-
+      
       
       
       
@@ -36,51 +36,62 @@ struct QuizView: View {
   }
 }
 struct AnswerSelection: View {
-    var body: some View {
-        let point = unitCirclePoints[0]
-        let scale: CGFloat = 0.4
-        let originalWidth: CGFloat = 120
-        let originalHeight: CGFloat = 45
-        let scaledWidth = originalWidth * scale
-        let scaledHeight = originalHeight * scale
-        
-        Canvas { context, size in
-            context.scaleBy(x: scale, y: scale)
-            if let pointSymbol = context.resolveSymbol(id: "pointView") {
-                context.draw(pointSymbol, in: CGRect(origin: .zero, size: CGSize(width: originalWidth, height: originalHeight)))
+  var body: some View {
+    Grid(horizontalSpacing: 5, verticalSpacing: 5) {
+      ForEach(0..<4, id: \.self) { row in
+        GridRow {
+          ForEach(0..<4, id: \.self) { column in
+            let index = row * 4 + column
+            if index < unitCirclePoints.count {
+              ScaledUnitCirclePoint(point: unitCirclePoints[index])
+            } else {
+              Color.clear
             }
-        } symbols: {
-            PointView(point: point)
-                .border(.green)
-                .frame(width: originalWidth, height: originalHeight)
-                .tag("pointView")
+          }
         }
-        .frame(width: scaledWidth, height: scaledHeight)
-        .border(.red)
+      }
     }
+    .padding()
+  }
 }
 
-extension Array {
-    func chunked(into size: Int) -> [[Element]] {
-        var chunks: [[Element]] = []
-        for index in stride(from: 0, to: count, by: size) {
-            let chunk = Array(self[index..<Swift.min(index + size, count)])
-            chunks.append(chunk)
-        }
-        return chunks
+struct ScaledUnitCirclePoint: View {
+  let point: UnitCirclePoint
+  var body: some View {
+    
+    let scale: CGFloat = 0.34
+    let originalWidth: CGFloat = 250
+    let originalHeight: CGFloat = 100
+    let scaledWidth = originalWidth * scale
+    let scaledHeight = originalHeight * scale
+    
+    Canvas { context, size in
+      context.scaleBy(x: scale, y: scale)
+      if let pointSymbol = context.resolveSymbol(id: "pointView") {
+        context.draw(pointSymbol, in: CGRect(origin: .zero, size: CGSize(width: originalWidth, height: originalHeight)))
+      }
+    } symbols: {
+      PointView(point: point)
+        .border(.green)
+        .frame(width: originalWidth, height: originalHeight)
+        .tag("pointView")
     }
+    .frame(width: scaledWidth, height: scaledHeight)
+    .border(.red)
+  }
 }
+
 
 struct TinyButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-        .controlSize(.mini)
-            .padding(5)
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(5)
-            .scaleEffect(configuration.isPressed ? 0.95 : 1)
-    }
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .controlSize(.mini)
+      .padding(5)
+      .background(Color.blue)
+      .foregroundColor(.white)
+      .cornerRadius(5)
+      .scaleEffect(configuration.isPressed ? 0.95 : 1)
+  }
 }
 
 
